314504a779958e62135e14254cc58e4558476246
paul <->
2022-01-27 22:44:48 +0100

    Added files

---
+++ src/main/java/com/example/sudoku/Sudoku.java
@@ -0,0 +1,402 @@
+package com.example.sudoku;
+
+import java.util.*;
+
+public class Sudoku {
+    //Sudoku Array
+    private Kachel[][] sudoku = new Kachel[9][9];
+
+    //counts the number of returns in the solveSudoku Method
+    private int returns = 0;
+
+    //Konstruktor
+    public Sudoku() {
+        //Insert Kacheln
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sudoku[i][j] = new Kachel(i,j);
+            }
+        }
+    }
+
+    //Solve the sudoku
+    public Integer[][] sudokusolver(Integer[][] pinput) throws Exception{
+        //Get Sudoku
+        sudokuanlegen(pinput);
+        //solve sudoku
+        if (solveSudoku()) {
+            return sudokuausgeben(sudoku);
+        } else {
+            throw new Exception("not possible");
+        }
+    }
+
+    public void kachelsudokusolver(Kachel[][] pinput){
+        sudoku = pinput;
+        solveSudoku();
+    }
+
+    //Possibilities
+    public Integer[][][] possibilities(Integer[][] pint){
+        //3d array to store the possible numbers in the array (playing 3d chess move)
+        Integer[][][] pintnumbers = new Integer[9][9][9];
+        //Instances the field
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                for (int k = 0; k < 9; k++) {
+                    pintnumbers[i][j][k] = 0;
+                }
+            }
+        }
+        //Kacheln erstellen
+        sudokuanlegen(pint);
+        deletenums();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                //Adding numbers
+                ArrayList<Integer> pnumbers = sudoku[i][j].getnumbers();
+                for (int k = 0; k < pnumbers.size(); k++) {
+                    pintnumbers[i][j][k] = pnumbers.get(k);
+                }
+            }
+        }
+        return pintnumbers;
+    }
+
+    public Integer getreturns(){
+        return returns;
+    }
+
+    // Integer Array --> Kachel Array
+    private void sudokuanlegen(Integer[][] intsudoku) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                //Checks if the numbers are 1-9
+                if (Objects.nonNull(intsudoku[i][j])) {
+                    if (intsudoku[i][j] > 0 && intsudoku[i][j] < 10) {
+                        sudoku[i][j].setnum(intsudoku[i][j]);
+                    } else { //somewhat redundant but avoids null pointers
+                        sudoku[i][j].setnum(0);
+                    }
+                }
+                else{
+                    sudoku[i][j].setnum(0);
+                }
+            }
+        }
+    }
+
+    //Kachel Array --> Integer Array
+    private Integer[][] sudokuausgeben(Kachel[][] psudoku) {
+        Integer[][] pintsudoku = new Integer[9][9];
+        for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < 9; i++) {
+                if(psudoku[i][j].getnum()!=0) {
+                    pintsudoku[i][j] = psudoku[i][j].getnum();
+                }
+                else{
+                    pintsudoku[i][j] = 0;
+                }
+            }
+        }
+        return pintsudoku;
+    }
+
+    /*
+        Working Sudoku solver (:
+     */
+
+    //finds empty fields in the sudoku
+    private coords findemptiness(Kachel[][] psudoku) {
+        for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < 9; i++) {
+                if (psudoku[j][i].getnum() == 0) {
+                    return (new coords(j, i));
+                }
+            }
+        }
+        return new coords();
+    }
+
+    private boolean solveSudoku() {
+        //gimme gimme gimme an empty field
+        coords c = findemptiness(sudoku);
+        // No empty spaces left? If yes, the job is done
+        if (c.getdone()) {
+            returns ++;
+            return true;
+        }
+        //now lets get the row and column from the empty field
+        int row = c.getrow();
+        int column = c.getcolumn();
+        // backtracking
+        for (int num = 1; num<=9;num++) {
+            if (valid(sudoku, row, column,num)) {
+                sudoku[row][column].setnum(num);
+                //Rekursion
+                if (solveSudoku()) {
+                    returns ++;
+                    return true;
+                }
+                //Replaces the made mistakes with 0, so it can work on it again
+                else {
+                    sudoku[row][column].setnum(0);
+                }
+            }
+        }
+        returns ++;
+        return false;
+    }
+
+    private static boolean valid(Kachel[][] psudoku, int row, int column, Integer num) {
+        // Check row
+        for (int d = 0; d < psudoku.length; d++) {
+            if (Objects.equals(psudoku[row][d].getnum(), num)) {
+                return false;
+            }
+        }
+
+        // Check the Column
+        for (Kachel[] kachels : psudoku) {
+            if (Objects.equals(kachels[column].getnum(), num)) {
+                return false;
+            }
+        }
+
+        //Check quadrant
+        int sqrt = (int) Math.sqrt(psudoku.length);
+        int boxRowStart = row - row % sqrt;
+        int boxColStart = column - column % sqrt;
+
+        for (int r = boxRowStart; r < boxRowStart + sqrt; r++) {
+            for (int d = boxColStart; d < boxColStart + sqrt; d++) {
+                if (Objects.equals(psudoku[r][d].getnum(), num)) {
+                    return false;
+                }
+            }
+        }
+        //Seems legit
+        return true;
+    }
+
+    public Integer[][] generatesudoku(int difficulty){
+        //Gets a random Quadrant
+        int qrow = (int)(Math.random()*((3-1)+1))+1;
+        int qcolumn = (int)(Math.random()*((3-1)+1))+1;
+        qrow = qrow * 3 - 3;
+        qcolumn = qcolumn * 3 - 3;
+
+        //DEBUG
+        System.out.println("Quadrant:" + qrow + " ; " + qcolumn);
+
+        //List cointaining 1-9
+        List<Integer> plist = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        //counts the runs
+        int count = 0;
+        for(int i = 0; i < 3 ; i++){
+            for (int j = 0; j < 3; j++) {
+                int num = plist.get((int)(Math.random()*((plist.size()))));
+
+                //DEBUG
+                System.out.println("Picked: " + num + "From the list, in run:" + count + "  Coordinates: row: " + i + " column: " + j);
+
+                plist.remove(Integer.valueOf(num));
+                sudoku[qrow+i][qcolumn+j].setnum(num);
+            }
+        }
+        //Places the backup
+        //sudoku = psudoku;
+
+
+        //Difficulty comes in place
+        int min = 20 + 11*difficulty;
+        int max = 30 + 15*difficulty;
+        int amount = (int)(Math.random()*((max-min)+1))+min;
+        System.out.println("min:" + min + " max:" + max + " amount:" + amount);
+        //solves it
+        solveSudoku();
+        //Deletes Numbers from it
+        ArrayList<String> pdeleted = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            int prandomrow = (int)(Math.random()*((9)));
+            int prandomcolumn = (int)(Math.random()*((9)));
+            String pcombined = prandomrow + String.valueOf(prandomcolumn);
+            if(!pdeleted.contains(pcombined)){
+                sudoku[prandomrow][prandomcolumn].setnum(0);
+                pdeleted.add(0, prandomrow + String.valueOf(prandomcolumn));
+            }
+            else{
+                i--;
+            }
+        }
+
+
+        /*
+        boolean difficultyright = true;
+
+        while (difficultyright){
+            solveSudoku();
+            int min = 20;
+            int max = 70;
+            int amount = (int)(Math.random()*((max-min)+1))+min;
+            //Deletes Numbers from it
+            ArrayList<String> pdeleted = new ArrayList<>();
+            for (int i = 0; i < amount; i++) {
+                int prandomrow = (int)(Math.random()*((9)));
+                int prandomcolumn = (int)(Math.random()*((9)));
+                String pcombined = prandomrow + String.valueOf(prandomcolumn);
+                if(!pdeleted.contains(pcombined)){
+                    sudoku[prandomrow][prandomcolumn].setnum(0);
+                    pdeleted.add(0, prandomrow + String.valueOf(prandomcolumn));
+                }
+                else{
+                    i--;
+                }
+            }
+
+            Sudoku psudoku = new Sudoku();
+            psudoku.kachelsudokusolver(sudoku);
+            Integer integer = psudoku.getreturns();
+            if(difficulty == 0 && integer < 100){
+                difficultyright = false;
+            }
+            else if(difficulty == 1 && integer < 100){
+                difficultyright = false;
+            }
+            else if(difficulty == 2 && integer < 300){
+                difficultyright = false;
+            }
+            else if(difficulty == 3 && integer < 500){
+                difficultyright = false;
+            }
+        }
+        */
+
+
+
+        //Turns it into integer Array
+        Integer[][] intsudoku = new Integer[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                intsudoku[i][j] = sudoku[i][j].getnum();
+            }
+        }
+        return intsudoku;
+    }
+
+    /*
+            Gets the possible numbers for every field, may be used later on
+     */
+    private void deletenums() {
+        for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < 9; i++) {
+                Integer num = sudoku[j][i].getnum();
+                if (num != 0) {
+                    //removes possible numbers from the lists in the quadrant
+                    removeinQ(j, i);
+                    //remove possible numbers from the rows and column
+                    removeZS(j, i, num);
+                }
+            }
+        }
+    }
+
+    /*
+        Row and column
+     */
+    private void removeZS(int row, int column, Integer num) {
+        //Remove in Row
+        for (int i = 0; i < 9; i++) {
+            sudoku[i][column].removenumbers(num);
+        }
+        //Remove in Column
+        for (int i = 0; i < 9; i++) {
+            sudoku[row][i].removenumbers(num);
+        }
+    }
+
+    /*
+        quadrant
+     */
+    private void removeinQ(int row, int column) {
+        int num = sudoku[row][column].getnum();
+        switch (row) {
+            case 0, 1, 2:
+                switch (column) {
+                    case 0, 1, 2:
+                        for (int j = 0; j < 3; j++) {
+                            for (int i = 0; i < 3; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                    case 3, 4, 5:
+                        for (int j = 0; j < 3; j++) {
+                            for (int i = 3; i < 6; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                    case 6, 7, 8:
+                        for (int j = 0; j < 3; j++) {
+                            for (int i = 6; i < 9; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                }
+                break;
+            case 3, 4, 5:
+                switch (column) {
+                    case 0, 1, 2:
+                        for (int j = 3; j < 6; j++) {
+                            for (int i = 0; i < 3; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                    case 3, 4, 5:
+                        for (int j = 3; j < 6; j++) {
+                            for (int i = 3; i < 6; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                    case 6, 7, 8:
+                        for (int j = 3; j < 6; j++) {
+                            for (int i = 6; i < 9; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                }
+                break;
+            case 6, 7, 8:
+                switch (column) {
+                    case 0, 1, 2:
+                        for (int j = 6; j < 9; j++) {
+                            for (int i = 0; i < 3; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                    case 3, 4, 5:
+                        for (int j = 6; j < 9; j++) {
+                            for (int i = 3; i < 6; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                    case 6, 7, 8:
+                        for (int j = 6; j < 9; j++) {
+                            for (int i = 6; i < 9; i++) {
+                                sudoku[j][i].removenumbers(num);
+                            }
+                        }
+                        break;
+                }
+                break;
+        }
+    }
+}
